451de2a80c6da15b63b326ece324b890fe9dd775
Alfredo Concha <punchi10@hotmail.com>
2015-09-28 18:47:08 -0300

    mismo orden de laravel 5.1

--- es_ES/passwords.php
+++ es_ES/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    "password" => "La contraseña debe tener al menos 6 caracteres y coincidir con la confirmación.",
-    "user" => "No se ha encontrado un usuario con esa dirección de correo.",
-    "token" => "Este token de reestablecimiento de contraseña es inválido.",
-    "sent" => "¡Recordatorio de contraseña enviado!",
-    "reset" => "¡Su contraseña ha sido reestablecida!",
+    'password' => 'La contraseña debe tener al menos 6 caracteres y coincidir con la confirmación.',
+    'reset' => '¡Su contraseña ha sido reestablecida!',
+    'sent' => '¡Recordatorio de contraseña enviado!',
+    'token' => 'Este token de reestablecimiento de contraseña es inválido.',
+    'user' => 'No se ha encontrado un usuario con esa dirección de correo.',
 
 ];
