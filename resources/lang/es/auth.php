148bf00418cf6803fc644cd415e6810debe735a5
Markski <immarkski@live.com>
2018-10-01 21:55:17 -0300

    Update auth.php

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -14,6 +14,6 @@ return [
     */
 
     'failed' => 'Estas credenciales no coinciden con nuestros registros.',
-    'throttle' => 'Demasiados intentos de inicio de sesión. Vuelva a intentarlo en :seconds segundos.',
+    'throttle' => 'Demasiados intentos fallidos en muy poco tiempo. Por favor intente de nuevo en :seconds segundos.',
 
 ];
