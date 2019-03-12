6b41bc337654f53b8afd20334eaca21c92d29543
José Loarca <joseloarca97@icloud.com>
2019-03-12 09:48:58 -0600

    Corrección validación contraseña
    Se actualizó el mensaje de validación de contraseña para ser compatible con Laravel 5.8.

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'password' => 'La contraseña debe tener al menos seis caracteres y coincidir con la confirmación.',
+    'password' => 'La contraseña debe tener al menos ocho caracteres y coincidir con la confirmación.',
     'reset' => '¡Su contraseña ha sido restablecida!',
     'sent' => '¡Recordatorio de contraseña enviado!',
     'token' => 'Este token de restablecimiento de contraseña es inválido.',
