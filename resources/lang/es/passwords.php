fcab81d360fb0c80bfb3f370552b4a143424b51f
David Fernández <david@mundosaparte.com>
2020-05-04 14:02:46 +0200

    traducción faltante

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -18,5 +18,5 @@ return [
     'token' => 'Este token de restablecimiento de contraseña es inválido.',
     'user' => 'No se ha encontrado un usuario con esa dirección de correo.',
     'throttled' => 'Por favor espere antes de volver a intentarlo.',
-
+    'password' => 'Las contraseñas deben tener al menos seis caracteres y coincidir con la confirmación.'
 ];
