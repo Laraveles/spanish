6de1482aaa6a13b4ae2ca91361ae1e3cf6e4e896
Guillermo Agudelo <guille.agudelo@gmail.com>
2021-03-25 15:51:51 -0500

    Update auth.php
    Actualiza traducciones auth Laravel 8

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -14,6 +14,7 @@ return [
     */
 
     'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña ingresada es incorrecta.',
     'throttle' => 'Demasiados intentos de acceso. Por favor inténtelo de nuevo en :seconds segundos.',
 
 ];
