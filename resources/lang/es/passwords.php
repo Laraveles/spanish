d169d6b7f7ac84966616d18ef3ac5656067204db
Cindy Mendoza <mendozaic17@gmail.com>
2022-12-26 21:44:54 -0500

    updated to version 9

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -15,8 +15,7 @@ return [
 
     'reset' => '¡Su contraseña ha sido restablecida!',
     'sent' => '¡Recordatorio de contraseña enviado!',
+    'throttled' => 'Por favor espere antes de volver a intentarlo.',
     'token' => 'Este token de restablecimiento de contraseña es inválido.',
     'user' => 'No se ha encontrado un usuario con esa dirección de correo.',
-    'throttled' => 'Por favor espere antes de volver a intentarlo.',
-    'password' => 'Las contraseñas deben tener al menos seis caracteres y coincidir con la confirmación.'
 ];
