bbab0f8d7b912ddca48b589b34b48488455c26fd
miang <miguelamadormj@gmail.com>
2023-02-17 17:20:08 -0500

    Agregando traduccion a incorrect password feedback

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -14,6 +14,7 @@ return [
     */
 
     'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña proporcionada es incorrecta.',
     'throttle' => 'Demasiados intentos de acceso. Por favor inténtelo de nuevo en :seconds segundos.',
 
 ];
